7e45a6e99c02123e0cf9286b1d44adf3744dab88
Leonardo250 <leomoulin2002@gmail.com>
2025-07-27 17:36:11 -0300

    first commit

---
+++ src/main/java/dev/java/CadastroDePessoas/PrimeiroContatoApplication.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroDePessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroContatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroContatoApplication.class, args);
+	}
+
+}
