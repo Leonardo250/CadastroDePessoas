92aea793004eb7eb64343c28cd1ae6c47bee73d5
Leonardo250 <leomoulin2002@gmail.com>
2025-08-01 16:11:10 -0300

    atualizado

--- src/main/java/dev/java/CadastroDePessoas/Pessoa/PessoaModel.java
+++ src/main/java/dev/java/CadastroDePessoas/Pessoa/PessoaModel.java
@@ -2,6 +2,9 @@ package dev.java.CadastroDePessoas.Pessoa;
 
 import dev.java.CadastroDePessoas.Trabalhos.CarrosModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,6 +12,9 @@ import java.util.List;
 // JPA = java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data // cria os getters e setters
+@NoArgsConstructor
+@AllArgsConstructor
 public class PessoaModel {
 
     @Id
@@ -23,37 +29,7 @@ public class PessoaModel {
     @JoinColumn(name = "Carros_id") // chave estrangeira para conectar as tabelas
     private CarrosModel Carros;
 
-    public PessoaModel() {
 
-    }
 
-    public PessoaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
