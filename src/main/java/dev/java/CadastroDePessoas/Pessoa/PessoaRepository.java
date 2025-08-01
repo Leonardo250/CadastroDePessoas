5b4dda5505c222290fdebdb62bbcbba642d7f61e
Leonardo250 <leomoulin2002@gmail.com>
2025-08-01 19:36:38 -0300

    Projeto Concluido

--- src/main/java/dev/java/CadastroDePessoas/Pessoa/PessoaRepository.java
+++ src/main/java/dev/java/CadastroDePessoas/Pessoa/PessoaRepository.java
@@ -1,3 +1,5 @@
 package dev.java.CadastroDePessoas.Pessoa;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface PessoaRepository extends JpaRepository<PessoaModel, Long> {}
